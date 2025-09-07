0e803efc42c7ff3b38d09857fdead77c02df9741
brayan <brayan@hotmail.com>
2025-09-07 17:33:30 -0500

    adding aplicationProperties and docker-compose

---
+++ system-app-backend/src/main/java/com/company/systemappbackend/SystemAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.systemappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemAppBackendApplication.class, args);
+    }
+
+}
---
+++ system-app-backend/src/test/java/com/company/systemappbackend/SystemAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.systemappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
